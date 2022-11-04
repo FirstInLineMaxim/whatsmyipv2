de0866d802acda772a9716de16160c03f34e193e
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-04 19:00:32 +0100

    all of that

--- src/App.js
+++ src/App.js
@@ -1,29 +1,63 @@
 import React,{useEffect,useState} from 'react';
 import './App.css';
 import LeafletMap from './LeafletMap';
-
+// import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function App() {
   const [IP, setIP] = useState()
   useEffect(() => {
-    fetch(process.env.REACT_APP_API_URL).then(response => response.json()).then(data => setIP(data))
-  }, [])
-
-  const [country, setCountry] = useState()
-  useEffect(()=>{
-    if(IP){
-      fetch(`https://restcountries.com/v3.1/alpha?codes=${IP.location.country}`).then(response => response.json()).then(data => setCountry(data))
-    }
+      fetch(process.env.REACT_APP_API_URL).then(response => response.json()).then(data => setIP(data))
+    }, [])
     
-  },[IP])
-  
-  
+    const [country, setCountry] = useState()
+    useEffect(()=>{
+        if(IP){
+            fetch(`https://restcountries.com/v3.1/alpha?codes=${IP.location.country}`).then(response => response.json()).then(data => setCountry(data))
+          }
+        
+        },[IP])
+        
+        const { DateTime } = require("luxon");
+
   return (
     <>
-    {country && <img src={country[0].flags.png} alt="flag"/>}
-    {IP && <p>{IP.ip}</p>}
-    <LeafletMap/>
+    {/* <Button>FUnFacts</Button> */}
+<div className='map_card'>
+ { country &&  <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="flag"
+        height="auto"
+        image={country[0].flags.png}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        {IP && <p>Your Ip {IP.ip}</p>}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        {country && <>
+        <p>Today is the {DateTime.now().setLocale(country[0].cca2).toLocaleString(DateTime.DATE_MED)}</p>
+        <p>{country[0].region}</p>
+        </>
+        }
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>}
+    
+    {/*IP.location  */}
 
+    {country && <LeafletMap data={IP.location}/>}
+    </div>
     </>
   );
 }
--- src/LeafletMap.jsx
+++ src/LeafletMap.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { MapContainer, TileLayer,Marker,Popup } from 'react-leaflet'
 
-export default function LeafletMap() {
+export default function LeafletMap({data}) {
+  console.log(data.lat)
   return (
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[data.lat,data.lng]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={[data.lat, data.lng]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
