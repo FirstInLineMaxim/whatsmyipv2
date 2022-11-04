0a2ef4187b10fe85e1ba7b8034a07167fbb11b51
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-04 18:00:15 +0100

    added flag fetching

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React,{useEffect,useState} from 'react';
 import './App.css';
 import LeafletMap from './LeafletMap';
 
 
 function App() {
+  const [IP, setIP] = useState()
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL).then(response => response.json()).then(data => setIP(data))
+  }, [])
+
+  const [country, setCountry] = useState()
+  useEffect(()=>{
+    if(IP){
+      fetch(`https://restcountries.com/v3.1/alpha?codes=${IP.location.country}`).then(response => response.json()).then(data => setCountry(data))
+    }
+    
+  },[IP])
+  
+  
   return (
     <>
-    <p>Hello WOrld</p>
+    {country && <img src={country[0].flags.png} alt="flag"/>}
+    {IP && <p>{IP.ip}</p>}
     <LeafletMap/>
-    <p>Hello WOrld</p>
 
     </>
   );
